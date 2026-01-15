a4505ff013fe6dd67bd1d282786a6311e1d11c6b
Molly-EA111 <ekonrado85@gmail.com>
2026-01-14 21:58:24 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,94 +1,25 @@
-// SELECTORES
-var mainSliderSelector = '.main-slider';
-var navSliderSelector = '.nav-slider';
-var interleaveOffset = 0.5;
-
-// SLIDER PRINCIPAL
-var mainSlider = new Swiper(mainSliderSelector, {
+var mainSlider = new Swiper('.main-slider', {
   loop: true,
-  speed: 1000,
-  grabCursor: true,
-  watchSlidesProgress: true,
-
+  speed: 1200,
   autoplay: {
-    delay: 3000,
+    delay: 6000, // ⏱️ MÁS TIEMPO POR SLIDE
     disableOnInteraction: false
   },
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
-  },
-
-  on: {
-    init: function () {
-      // Forzar autoplay seguro en iframe
-      var swiper = this;
-      setTimeout(function () {
-        swiper.autoplay.start();
-      }, 500);
-    },
-
-    slideChangeTransitionEnd: function () {
-      var swiper = this;
-      var captions = swiper.el.querySelectorAll('.caption');
-
-      captions.forEach(function (el) {
-        el.classList.remove('show');
-      });
-
-      var activeCaption = swiper.slides[swiper.activeIndex].querySelector('.caption');
-      if (activeCaption) {
-        activeCaption.classList.add('show');
-      }
-    },
-
-    progress: function () {
-      var swiper = this;
-
-      swiper.slides.forEach(function (slide) {
-        var slideProgress = slide.progress;
-        var innerOffset = swiper.width * interleaveOffset;
-        var innerTranslate = slideProgress * innerOffset;
-
-        var bg = slide.querySelector('.slide-bgimg');
-        if (bg) {
-          bg.style.transform = 'translateX(' + innerTranslate + 'px)';
-        }
-      });
-    },
-
-    touchStart: function () {
-      this.slides.forEach(function (slide) {
-        slide.style.transition = '';
-      });
-    },
-
-    setTransition: function (speed) {
-      this.slides.forEach(function (slide) {
-        slide.style.transition = speed + 'ms';
-
-        var bg = slide.querySelector('.slide-bgimg');
-        if (bg) {
-          bg.style.transition = speed + 'ms';
-        }
-      });
-    }
   }
 });
 
-// SLIDER DE NAVEGACIÓN
-var navSlider = new Swiper(navSliderSelector, {
-  loop: true,
-  speed: 1000,
-  spaceBetween: 5,
-  slidesPerView: 5,
-  centeredSlides: true,
-  slideToClickedSlide: true,
+var navSlider = new Swiper('.nav-slider', {
   direction: 'vertical',
+  slidesPerView: 12,
+  spaceBetween: 2,
+  centeredSlides: false,
+  slideToClickedSlide: true,
+  watchSlidesVisibility: true,
   watchSlidesProgress: true
 });
 
-// SINCRONIZAR SLIDERS
 mainSlider.controller.control = navSlider;
 navSlider.controller.control = mainSlider;
