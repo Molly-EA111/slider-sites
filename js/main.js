cca49e2cb8fa0486e7fc0038f89e2b3762eeccf3
Molly-EA111 <ekonrado85@gmail.com>
2026-01-14 22:31:04 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
-var slider = new Swiper('.main-slider', {
+const totalSlides = 12;
+const startIndex = 2;
+const sliderWrapper = document.querySelector('.swiper-wrapper');
+
+// 🔁 GENERAR SLIDES AUTOMÁTICAMENTE
+for (let i = startIndex; i < startIndex + totalSlides; i++) {
+  const slide = document.createElement('div');
+  slide.className = 'swiper-slide';
+
+  slide.innerHTML = `
+    <div class="slide-bgimg" style='background-image:url("images/img(${i}).png")'></div>
+  `;
+
+  sliderWrapper.appendChild(slide);
+}
+
+// 🚀 INICIALIZAR SWIPER
+new Swiper('.main-slider', {
   loop: true,
-  loopAdditionalSlides: 12,
-  speed: 1500,
+  loopAdditionalSlides: totalSlides,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  speed: 2000,
   autoplay: {
-    delay: 7000, // ⏱️ más tiempo por slide
+    delay: 8000,          // ⏱️ más tiempo por imagen
     disableOnInteraction: false
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  }
+  allowTouchMove: true
 });
